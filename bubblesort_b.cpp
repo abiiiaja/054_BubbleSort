d040409afa4b12595a1410eeb1cbcee947e1c4d3
abiiiaja <kalindraaraya@gmail.com>
2026-03-05 17:43:00 +0700

    Menambahkan prosedur pertama: Menentukan berapa banyak array yang dibutuhkan

--- bubblesort_b.cpp
+++ bubblesort_b.cpp
@@ -4,3 +4,17 @@ using namespace std;
 
 int arr[20]; //Deklarasi array global dengan ukuran 20
 int n; //Deklarasi variabel n dengan tipe data integer
+
+void input(){
+    int d;
+    while (true){
+        cout << "Mau barapa elemen pada array? : ";
+        cin >> n;
+        if (n<=20)
+            break;
+        else
+            cout << "\nTerlalu banyak, maksimal 20 saja\n";
+
+    }
+}
+
