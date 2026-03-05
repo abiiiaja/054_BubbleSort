db859b6cdc0838995b0b8d2b9fc66fbfb799b10a
abiiiaja <kalindraaraya@gmail.com>
2026-03-05 17:49:34 +0700

    Masih di prosedur input: Menambahkan data ke array-nya

--- bubblesort_b.cpp
+++ bubblesort_b.cpp
@@ -5,16 +5,26 @@ using namespace std;
 int arr[20]; //Deklarasi array global dengan ukuran 20
 int n; //Deklarasi variabel n dengan tipe data integer
 
-void input(){
+void input(){ //Prosedutr input
     int d;
     while (true){
         cout << "Mau barapa elemen pada array? : ";
         cin >> n;
         if (n<=20)
             break;
-        else
-            cout << "\nTerlalu banyak, maksimal 20 saja\n";
+        else{
+            cout << "\nTerlalu banyak, maksimal 20 saja\n";}
 
     }
+    
+cout << endl;
+cout << "======================" << endl;
+cout << "Masukkan elemen array" << endl;
+cout << "======================" << endl;
+
+for (int i=0; i<n; i++){
+    cout << "Data ke-" << (i+1) << ": ";
+    cin >> arr[i];}
+
 }
 
